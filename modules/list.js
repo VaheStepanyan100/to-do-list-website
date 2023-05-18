4391058e4fae76c118d5fed45e3647aab9a05caa
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-05-18 13:12:59 +0400

    Update some files

--- modules/list.js
+++ modules/list.js
@@ -2,11 +2,11 @@ export default (container, task, trash) => {
   const listItem = document.createElement('li');
   listItem.className = 'list-item';
 
-  listItem.innerHTML = `<input class="check-box" type="checkbox" ${
-    task.completed ? 'checked' : 'unchecked'
-  }>
-    <p class="description" contenteditable="true">${task.description}</p>
-    <a class="delete-icon"><img class="delete-img" src="${trash}" alt="trash"</a>
-  `;
+  // listItem.innerHTML = `<input class="check-box" type="checkbox" ${
+  //   task.completed ? 'checked' : 'unchecked'
+  // }>
+  //   <p class="description" contenteditable="true">${task.description}</p>
+  //   <a class="delete-icon"><img class="delete-img" src="${trash}" alt="trash"</a>
+  // `;
   container.appendChild(listItem);
 };
